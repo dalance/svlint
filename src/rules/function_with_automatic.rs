e35e93bb6bb36c2caaa1ecf9381449b698256f81
dalance <dalance@gmail.com>
2020-02-19 15:06:45 +0900

    Support default lifetime #13

--- src/rules/function_with_automatic.rs
+++ src/rules/function_with_automatic.rs
@@ -1,23 +1,115 @@
 use crate::linter::{Rule, RuleResult};
-use sv_parser::{Lifetime, NodeEvent, RefNode, SyntaxTree};
+use sv_parser::{NodeEvent, RefNode, SyntaxTree};
 
-pub struct FunctionWithAutomatic;
+#[derive(Default)]
+pub struct FunctionWithAutomatic {
+    lifetimes: Vec<Lifetime>,
+}
+
+enum Lifetime {
+    Static,
+    Automatic,
+}
 
 impl Rule for FunctionWithAutomatic {
     fn check(&mut self, _syntax_tree: &SyntaxTree, event: &NodeEvent) -> RuleResult {
-        let node = match event {
-            NodeEvent::Enter(x) => x,
-            NodeEvent::Leave(_) => {
-                return RuleResult::Skip;
+        match event {
+            NodeEvent::Enter(RefNode::ModuleAnsiHeader(x)) => {
+                let (_, _, ref a, _, _, _, _, _) = x.nodes;
+                match a {
+                    Some(sv_parser::Lifetime::Automatic(_)) => {
+                        self.lifetimes.push(Lifetime::Automatic)
+                    }
+                    Some(sv_parser::Lifetime::Static(_)) => self.lifetimes.push(Lifetime::Static),
+                    _ => (),
+                }
+                RuleResult::Pass
             }
-        };
-        match node {
-            RefNode::FunctionDeclaration(x) => {
+            NodeEvent::Enter(RefNode::ModuleNonansiHeader(x)) => {
+                let (_, _, ref a, _, _, _, _, _) = x.nodes;
+                match a {
+                    Some(sv_parser::Lifetime::Automatic(_)) => {
+                        self.lifetimes.push(Lifetime::Automatic)
+                    }
+                    Some(sv_parser::Lifetime::Static(_)) => self.lifetimes.push(Lifetime::Static),
+                    _ => (),
+                }
+                RuleResult::Pass
+            }
+            NodeEvent::Enter(RefNode::InterfaceAnsiHeader(x)) => {
+                let (_, _, ref a, _, _, _, _, _) = x.nodes;
+                match a {
+                    Some(sv_parser::Lifetime::Automatic(_)) => {
+                        self.lifetimes.push(Lifetime::Automatic)
+                    }
+                    Some(sv_parser::Lifetime::Static(_)) => self.lifetimes.push(Lifetime::Static),
+                    _ => (),
+                }
+                RuleResult::Pass
+            }
+            NodeEvent::Enter(RefNode::InterfaceNonansiHeader(x)) => {
+                let (_, _, ref a, _, _, _, _, _) = x.nodes;
+                match a {
+                    Some(sv_parser::Lifetime::Automatic(_)) => {
+                        self.lifetimes.push(Lifetime::Automatic)
+                    }
+                    Some(sv_parser::Lifetime::Static(_)) => self.lifetimes.push(Lifetime::Static),
+                    _ => (),
+                }
+                RuleResult::Pass
+            }
+            NodeEvent::Enter(RefNode::ProgramAnsiHeader(x)) => {
+                let (_, _, ref a, _, _, _, _, _) = x.nodes;
+                match a {
+                    Some(sv_parser::Lifetime::Automatic(_)) => {
+                        self.lifetimes.push(Lifetime::Automatic)
+                    }
+                    Some(sv_parser::Lifetime::Static(_)) => self.lifetimes.push(Lifetime::Static),
+                    _ => (),
+                }
+                RuleResult::Pass
+            }
+            NodeEvent::Enter(RefNode::ProgramNonansiHeader(x)) => {
+                let (_, _, ref a, _, _, _, _, _) = x.nodes;
+                match a {
+                    Some(sv_parser::Lifetime::Automatic(_)) => {
+                        self.lifetimes.push(Lifetime::Automatic)
+                    }
+                    Some(sv_parser::Lifetime::Static(_)) => self.lifetimes.push(Lifetime::Static),
+                    _ => (),
+                }
+                RuleResult::Pass
+            }
+            NodeEvent::Enter(RefNode::ClassDeclaration(_)) => {
+                self.lifetimes.push(Lifetime::Automatic);
+                RuleResult::Pass
+            }
+            NodeEvent::Enter(RefNode::FunctionDeclaration(x)) => {
                 let (_, ref a, _) = x.nodes;
                 match a {
-                    Some(Lifetime::Automatic(_)) => RuleResult::Pass,
-                    _ => RuleResult::Fail,
+                    Some(sv_parser::Lifetime::Automatic(_)) => RuleResult::Pass,
+                    Some(sv_parser::Lifetime::Static(_)) => RuleResult::Fail,
+                    None => {
+                        if let Some(x) = self.lifetimes.last() {
+                            match x {
+                                Lifetime::Automatic => RuleResult::Pass,
+                                Lifetime::Static => RuleResult::Fail,
+                            }
+                        } else {
+                            RuleResult::Fail
+                        }
+                    }
                 }
+            }
+            NodeEvent::Leave(RefNode::ModuleDeclarationAnsi(_))
+            | NodeEvent::Leave(RefNode::ModuleDeclarationNonansi(_))
+            | NodeEvent::Leave(RefNode::InterfaceDeclarationAnsi(_))
+            | NodeEvent::Leave(RefNode::InterfaceDeclarationNonansi(_))
+            | NodeEvent::Leave(RefNode::ProgramDeclarationAnsi(_))
+            | NodeEvent::Leave(RefNode::ProgramDeclarationNonansi(_))
+            | NodeEvent::Leave(RefNode::ClassDeclaration(_)) => {
+                self.lifetimes.pop();
+                RuleResult::Pass
             }
             _ => RuleResult::Pass,
         }
