4c5a84ffb7c699e0a533f568a97e4d99ba47a2bb
Dave McEwan <dave.mcewan@bristol.ac.uk>
2021-11-22 23:46:57 +0000

    generate_if_with_label Additionally check for prefix_label.
    This breaks existing checks for labels without prefix.
    For original non-prefix behaviour add `prefix_label = ""` to config.

--- src/rules/generate_if_with_label.rs
+++ src/rules/generate_if_with_label.rs
@@ -1,6 +1,8 @@
 use crate::config::ConfigOption;
 use crate::linter::{Rule, RuleResult};
-use sv_parser::{unwrap_locate, GenerateBlock, NodeEvent, RefNode, SyntaxTree};
+use sv_parser::{
+    unwrap_locate, unwrap_node, GenerateBlock, Locate, NodeEvent, RefNode, SyntaxTree,
+};
 
 #[derive(Default)]
 pub struct GenerateIfWithLabel;
@@ -8,9 +10,9 @@ pub struct GenerateIfWithLabel;
 impl Rule for GenerateIfWithLabel {
     fn check(
         &mut self,
-        _syntax_tree: &SyntaxTree,
+        syntax_tree: &SyntaxTree,
         event: &NodeEvent,
-        _option: &ConfigOption,
+        option: &ConfigOption,
     ) -> RuleResult {
         let node = match event {
             NodeEvent::Enter(x) => x,
@@ -21,28 +23,77 @@ impl Rule for GenerateIfWithLabel {
         match node {
             RefNode::IfGenerateConstruct(x) => {
                 let (_, _, ref a, ref b) = x.nodes;
+
+                let id: Option<&Locate> = match unwrap_node!(*x, GenerateBlockIdentifier) {
+                    Some(RefNode::GenerateBlockIdentifier(id_)) => {
+                        unwrap_locate!(id_)
+                    }
+                    _ => None,
+                };
+
                 match a {
                     GenerateBlock::Multiple(x) => {
                         let (_, _, ref a, _, _, _) = x.nodes;
-                        if a.is_some() {
+
+                        let is_prefixed: bool = match &id {
+                            Some(x) => {
+                                syntax_tree
+                                    .get_str(*x)
+                                    .unwrap()
+                                    .starts_with(&option.prefix_label)
+                            }
+                            _ => false,
+                        };
+
+                        if a.is_some() && is_prefixed {
                             match b {
                                 Some((e, x)) => match x {
                                     GenerateBlock::Multiple(x) => {
                                         let (_, _, ref a, _, _, _) = x.nodes;
-                                        if a.is_some() {
-                                            RuleResult::Pass
-                                        } else {
-                                            // failed because a label of 'else' is not found
-                                            let locate = unwrap_locate!(e).unwrap();
-                                            RuleResult::FailLocate(*locate)
+
+                                        let else_locate = unwrap_locate!(e).unwrap();
+
+                                        match a {
+                                            Some((_, id_)) => {
+                                                let id: Option<&Locate> = unwrap_locate!(id_);
+                                                let is_prefixed: bool = match &id {
+                                                    Some(x) => {
+                                                        syntax_tree
+                                                            .get_str(*x)
+                                                            .unwrap()
+                                                            .starts_with(&option.prefix_label)
+                                                    }
+                                                    _ => false,
+                                                };
+
+                                                if is_prefixed {
+                                                    RuleResult::Pass
+                                                } else {
+                                                    // failed because a label of 'else' doesn't have prefix
+                                                    RuleResult::FailLocate(*else_locate)
+                                                }
+                                            }
+                                            _ => {
+                                                // failed because a label of 'else' is not found
+                                                RuleResult::FailLocate(*else_locate)
+                                            }
                                         }
                                     }
-                                    _ => RuleResult::Pass,
+                                    _ => {
+                                        if is_prefixed {
+                                            RuleResult::Pass
+                                        } else {
+                                            // failed because a label of 'if' doesn't have prefix
+                                            RuleResult::Fail
+                                        }
+                                    }
                                 },
+                                // there is no 'else' to have a label
                                 None => RuleResult::Pass,
                             }
                         } else {
                             // failed because a label of 'if' is not found
+                            // OR the label doesn't have prefix
                             RuleResult::Fail
                         }
                     }
