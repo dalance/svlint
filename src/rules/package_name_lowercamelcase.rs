c2d412d44e77e207ec26f4f9c61b4ee08db9a933
Dave McEwan <dave.mcewan@bristol.ac.uk>
2021-12-12 21:01:07 +0000

    package_name_lowercamelcase

---
+++ src/rules/package_name_lowercamelcase.rs
@@ -0,0 +1,62 @@
+use crate::config::ConfigOption;
+use crate::linter::{Rule, RuleResult};
+use sv_parser::{unwrap_locate, unwrap_node, Locate, NodeEvent, RefNode, SyntaxTree};
+
+#[derive(Default)]
+pub struct PackageNameLowercamelcase;
+
+impl Rule for PackageNameLowercamelcase {
+    fn check(
+        &mut self,
+        syntax_tree: &SyntaxTree,
+        event: &NodeEvent,
+        _option: &ConfigOption,
+    ) -> RuleResult {
+        let node = match event {
+            NodeEvent::Enter(x) => x,
+            NodeEvent::Leave(_) => {
+                return RuleResult::Pass;
+            }
+        };
+        match node {
+            RefNode::PackageIdentifier(x) => {
+                let id: Option<&Locate> = match unwrap_node!(*x, SimpleIdentifier) {
+                    Some(RefNode::SimpleIdentifier(id_)) => {
+                        unwrap_locate!(id_)
+                    }
+                    _ => None,
+                };
+
+                let is_uppercamelcase: bool = match &id {
+                    Some(x) => syntax_tree
+                        .get_str(*x)
+                        .unwrap()
+                        .chars()
+                        .nth(0)
+                        .unwrap()
+                        .is_ascii_lowercase(),
+                    _ => false,
+                };
+
+                if is_uppercamelcase {
+                    RuleResult::Pass
+                } else {
+                    RuleResult::Fail
+                }
+            }
+            _ => RuleResult::Pass,
+        }
+    }
+
+    fn name(&self) -> String {
+        String::from("package_name_lowercamelcase")
+    }
+
+    fn hint(&self, _option: &ConfigOption) -> String {
+        String::from(format!("Package name must begin with lowerCamelCase"))
+    }
+
+    fn reason(&self) -> String {
+        String::from("Naming convention simplifies audit.")
+    }
+}
