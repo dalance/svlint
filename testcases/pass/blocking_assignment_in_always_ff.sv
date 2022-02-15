412b1205d1661c54a114929fc363b63b2823c5d7
Dave McEwan <dave.mcewan@bristol.ac.uk>
2022-02-15 11:34:56 +0100

    commentControl Use blocking_assignment_in_always_ff as testcase.

--- testcases/pass/blocking_assignment_in_always_ff.sv
+++ testcases/pass/blocking_assignment_in_always_ff.sv
@@ -2,4 +2,9 @@ module A;
 always_ff begin
     x <= 0;
 end
+/* svlint off blocking_assignment_in_always_ff */
+always_ff begin
+    x = 0;
+end
+/* svlint on blocking_assignment_in_always_ff */
 endmodule
