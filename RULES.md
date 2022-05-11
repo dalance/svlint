7584af0a821033c9fa560049f4f3dadc8633d5f4
Dave McEwan <dave.mcewan@bristol.ac.uk>
2022-05-11 17:53:37 +0200

    explanation case_default

--- RULES.md
+++ RULES.md
@@ -74,11 +74,11 @@ The most relevant clauses of IEEE1800-2017 are:
 
 ### Hint
 
-`case` must have `default` in `always_comb` or `function`
+Use a `default` expression in `case` statements.
 
 ### Reason
 
-'not full case' causes mismatch between simulation and synthesis
+Incomplete case may cause simulation/synthesis mismatch in `always_comb` and `function`.
 
 ### Pass Example
 
@@ -117,7 +117,29 @@ endmodule
 
 ### Explanation
 
-TODO
+IEEE1800-2017 (clause 9.2.2.2) comments that tools should *warn* if an
+`always_comb` procedure infers memory.
+However, simulators and synthesis tools are not required to enforce that
+`always_comb` procedures only infer combinational logic.
+This allows for simulators and synthesis tools to interpret these procedures
+differently, which results in a mismatch between simulation and synthesis.
+
+An incomplete case statement may be interpreted as latched logic,
+e.g: `always_comb case (foo) '0: a = 5; endcase`.
+Only the case where `foo == 0` is specified, to update variable `a` to the
+value `5`.
+When `foo` is non-zero, this example may be interpreted in at least two ways:
+  - `a = 'x;` - As the new value is not specified, it is unknown.
+    A synthesis tool may allow node `a` to be undriven, or choose to drive
+    `a` equivalently to one of the explicitly specified case expressions.
+  - `a = a;` - As the new value is not specified, do not change `a`.
+    A synthesis tool may produce a latching circuit.
+
+The most relevant clauses of IEEE1800-2017 are:
+  - 9.2.2.2 Combinational logic `always_comb` procedure
+  - 12.5 Case statement
+  - 13.4 Functions
+
 
 ---
 ## `default_nettype_none`
--- src/rules/case_default.rs
+++ src/rules/case_default.rs
@@ -1,6 +1,7 @@
 use crate::config::ConfigOption;
 use crate::linter::{Rule, RuleResult};
 use sv_parser::{unwrap_locate, unwrap_node, AlwaysKeyword, NodeEvent, RefNode, SyntaxTree};
+use indoc::indoc;
 
 #[derive(Default)]
 pub struct CaseDefault;
@@ -60,14 +61,37 @@ impl Rule for CaseDefault {
     }
 
     fn hint(&self, _option: &ConfigOption) -> String {
-        String::from("`case` must have `default` in `always_comb` or `function`")
+        String::from("Use a `default` expression in `case` statements.")
     }
 
     fn reason(&self) -> String {
-        String::from("'not full case' causes mismatch between simulation and synthesis")
+        String::from("Incomplete case may cause simulation/synthesis mismatch in `always_comb` and `function`.")
     }
 
     fn explanation(&self) -> String {
-        String::from("TODO")
+        String::from(indoc!{"
+        IEEE1800-2017 (clause 9.2.2.2) comments that tools should *warn* if an
+        `always_comb` procedure infers memory.
+        However, simulators and synthesis tools are not required to enforce that
+        `always_comb` procedures only infer combinational logic.
+        This allows for simulators and synthesis tools to interpret these procedures
+        differently, which results in a mismatch between simulation and synthesis.
+
+        An incomplete case statement may be interpreted as latched logic,
+        e.g: `always_comb case (foo) '0: a = 5; endcase`.
+        Only the case where `foo == 0` is specified, to update variable `a` to the
+        value `5`.
+        When `foo` is non-zero, this example may be interpreted in at least two ways:
+          - `a = 'x;` - As the new value is not specified, it is unknown.
+            A synthesis tool may allow node `a` to be undriven, or choose to drive
+            `a` equivalently to one of the explicitly specified case expressions.
+          - `a = a;` - As the new value is not specified, do not change `a`.
+            A synthesis tool may produce a latching circuit.
+
+        The most relevant clauses of IEEE1800-2017 are:
+          - 9.2.2.2 Combinational logic `always_comb` procedure
+          - 12.5 Case statement
+          - 13.4 Functions
+        "})
     }
 }
