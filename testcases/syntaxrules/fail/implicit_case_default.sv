ec897a60587660a5fef172aa41dceea835aff046
Shreyas Chinnola <schinno@purdue.edu>
2024-03-19 22:14:07 -0400

    added another fail testcase

--- testcases/syntaxrules/fail/implicit_case_default.sv
+++ testcases/syntaxrules/fail/implicit_case_default.sv
@@ -7,6 +7,19 @@ endmodule
 ////////////////////////////////////////////////////////////////////////////////
 module M;
   always_comb begin
+    y = 0;
+    case(x)
+      1: y = 1;
+      2: begin 
+        z = 1;
+        w = 1;
+      end
+    endcase
+  end
+endmodule
+////////////////////////////////////////////////////////////////////////////////
+module M;
+  always_comb begin
     a = 0;
     case(x)
       1: b = 0;
