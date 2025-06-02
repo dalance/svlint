9ef64ac930e320ed5283b5b9f6a58cc639b47423
Shantanu Sinha <shantanu.p.sinha@gmail.com>
2025-06-01 23:19:03 -0400

    fixes dalance/svlint#293

--- testcases/syntaxrules/pass/module_identifier_matches_filename.sv
+++ testcases/syntaxrules/pass/module_identifier_matches_filename.sv
@@ -1,5 +1,10 @@
-module syntaxrules;
+module syntaxrules();
 endmodule 
+
+
+// Allowing Whitespace in Module Names
+module syntaxrules ();
+endmodule
 
 // This testcase, when executed, is called from a file named "syntaxrules.module_identifier_matches_filename.pass.1of1"
 // The rule matches all valid characters up until the first non-identifier (in this case, the period).
