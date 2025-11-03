1e749c8a1f32a733e7fbada3c4b35a707bfe8278
ds54e <17592097+ds54e@users.noreply.github.com>
2025-11-03 18:54:25 +0900

    Add location tracking to default_nettype_wire_at_end rule.

--- src/syntaxrules/default_nettype_wire_at_end.rs
+++ src/syntaxrules/default_nettype_wire_at_end.rs
@@ -1,10 +1,11 @@
 use crate::config::ConfigOption;
 use crate::linter::{SyntaxRule, SyntaxRuleResult};
-use sv_parser::{NodeEvent, RefNode, SyntaxTree};
+use sv_parser::{NodeEvent, RefNode, SyntaxTree, Locate, unwrap_locate};
 
 #[derive(Default)]
 pub struct DefaultNettypeWireAtEnd {
     last_nettype_value: Option<String>,
+    last_nettype_loc: Option<Locate>,
 }
 
 impl SyntaxRule for DefaultNettypeWireAtEnd {
@@ -19,29 +20,41 @@ impl SyntaxRule for DefaultNettypeWireAtEnd {
                 // Reset state when entering a new source file
                 if let RefNode::SourceText(_) = x {
                     self.last_nettype_value = None;
+                    self.last_nettype_loc = None;
                 }
 
-                // Capture the value of each `default_nettype` directive
+                // Capture the value and location of each `default_nettype` directive
                 if let RefNode::DefaultNettypeCompilerDirective(directive) = x {
                     let (_symbol, _keyword, default_nettype_value) = &directive.nodes;
                     if let Some(s) = syntax_tree.get_str_trim(default_nettype_value) {
                         self.last_nettype_value = Some(s.to_string());
+                        self.last_nettype_loc = unwrap_locate!(default_nettype_value).cloned();
                     } else {
                         // Fail if directive exists but its value cannot be parsed
+                        if let Some(loc) = unwrap_locate!(default_nettype_value) {
+                            return SyntaxRuleResult::FailLocate(loc.clone());
+                        }
                         return SyntaxRuleResult::Fail;
                     }
                 }
                 SyntaxRuleResult::Pass
             }
             NodeEvent::Leave(x) => {
+                // At the end of the source file, check the final effective nettype
                 if let RefNode::SourceText(_) = x {
                     match self.last_nettype_value.as_deref() {
-                        // At the end of the source file, check the final effective nettype
+                        // Pass if the final nettype is "wire"
                         Some("wire") => SyntaxRuleResult::Pass,
                         // Pass if no directive is present at all (rule not applicable)
                         None => SyntaxRuleResult::Pass,
                         // Fail for any other value (e.g., "none")
-                        _ => SyntaxRuleResult::Fail,
+                        Some(_) => {
+                            if let Some(loc) = &self.last_nettype_loc {
+                                SyntaxRuleResult::FailLocate(loc.clone())
+                            } else {
+                                SyntaxRuleResult::Fail
+                            }
+                        }
                     }
                 } else {
                     SyntaxRuleResult::Pass
